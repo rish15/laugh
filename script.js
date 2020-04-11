ba0914be24ef2920c040965c91dd6e815f4fb6b5
rish15 <rish.cr7mini@gmail.com>
2020-04-12 02:29:18 +0530

    Added favourite component - users can add favourite jokes - on click of favourite button content switches to favourites - changed UI - reworte the fetch data with async/await

--- script.js
+++ script.js
@@ -1,51 +1,86 @@
-// event Listeners
-document.getElementById('tenJokes').addEventListener('click', getTenRandomJokes)
-document.getElementById('favourites').addEventListener('click',getFavourites)
-function getTenRandomJokes() {
-//create Elements
-var loader = document.createElement("div")
-loader.className = "lds-dual-ring"
+const randUrl = "https://official-joke-api.appspot.com/random_ten"
+var favourites = []
 
-document.getElementById("randomJokes").appendChild(loader)
-
-fetch("https://official-joke-api.appspot.com/random_ten")
-	.then(res => {
-		return res.json()
-	})
-	.then(data =>{
-		let randomJokes = '<h4>Random Jokes<h4>'
-		document.getElementById("randomJokes").appendChild(loader)
-
-		data.forEach(element =>{
-		randomJokes += `
-				<ul class="list-group mb-3">
-					<li id="randomJokeList" class="list-group-item">
-					<button class="fav">fav</button>
-						Category : ${element.type}<br>
-						setup: ${element.setup}<br>
-						punchline: ${element.punchline}<br>
-					</li>
-				</ul>
-			`;
-		});
-
-	document.getElementById('randomJokes').innerHTML = randomJokes
-	document.getElementById('tenJokes').innerHTML = "refresh"
-	const favButton = document.querySelectorAll('.fav')
-	var favourites = []
-	if(favButton){
-		 favButton.forEach(function(el){
-			 el.addEventListener('click', function (e) {
-			 favourites.push(e.target.parentNode.innerText);
-			 showFav(favourites)
-     		});
-  		});	
-	}
-})
+const getTenRandomJokes = async() =>{
+	let res =   await fetch(randUrl)
+	let data = await res.json();
+	return data;
 }
 
-function getFavourites(e){
-	function showFav(fav){
-		console.log(fav)
+const randomJokes = async () =>{
+// APPEND LOADER ELEMENT TO DOM
+var loader = document.createElement("div")
+loader.className = "lds-dual-ring"
+document.getElementById("randomJokes").appendChild(loader)
+
+// Displaying random jokes
+let show = await getTenRandomJokes()
+let randomJokes = '<h4>Random Jokes<h4>'
+
+	show.forEach(element =>{
+	randomJokes += `
+		<ul id="x" class="list-group mb-3">
+			<li id="randomJokeList" class="list-group-item">
+			<button id="favButton"><img class="fav" src="./assets/plus.png" alt="addFav" width="30px" height="30px"></button>
+				${element.setup}
+				<hr>
+				${element.punchline}<br>
+			</li>
+		</ul>
+		`;
+	});
+document.getElementById('randomJokes').innerHTML = randomJokes
+document.getElementById('tenJokes').innerHTML = "refresh random Jokes"
+document.getElementById('favButton').addEventListener('click', addFavourites)
+}
+
+const addFavourites = (e) =>{
+const favButton = document.querySelectorAll('.fav')
+
+	if(favButton){
+		favButton.forEach(element =>{
+			element.addEventListener('click', e =>{
+			favourites.push(e.srcElement.offsetParent.innerText)
+			localStorage.setItem('favourites',favourites)
+			})
+		})
 	}
-} 
+}
+
+const getFavourites = () =>{
+	const fav= favourites 
+	var displayFavourites = '<h3> Your Favourite Jokes </h3>'
+	if(fav.length!=0){	
+	fav.forEach(element =>{
+	let x = element.split('\n')
+	displayFavourites += `
+		<ul class="list-group mb-3">
+			<li class="list-group-item">
+				${x[0]}<br>
+				<hr>
+				${x[1]}
+			</li>
+		</ul>
+		`;
+	});
+	document.getElementById('displayFavourites').innerHTML = displayFavourites 
+	window.location.hash = "displayFavourites";
+	}else{
+
+		document.getElementById('displayFavourites').innerHTML = 
+
+			'<h3>Your Favourite Jokes</h3><br>\
+			<div id="noFavFound" class="alert alert-danger" role="alert">\
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+					<span aria-hidden="true">&times;</span>\
+				</button>\
+				<h3>Sorry ! your favourites is empty</h3>\
+			</div>'
+
+		window.location.hash = "#noFavFound"
+	}
+}
+
+//EVENT LINSTENERS
+document.getElementById('tenJokes').addEventListener('click', randomJokes)
+document.getElementById('favourites').addEventListener('click',getFavourites)
